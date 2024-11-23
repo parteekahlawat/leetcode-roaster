8ec36c401aa72f52bbb15c05a77ae23713583090
parteekahlawat <prateekahlawat1223@gmail.com>
2024-11-24 01:03:38 +0530

    files

--- app.py
+++ app.py
@@ -11,48 +11,43 @@ load_dotenv(find_dotenv())
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = "API_KEY"
 
-# Initialize the model
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
-# Streamlit app header
-st.write("Leetcode Roaster - Get Ready to be Roasted ")
+st.title("Leetcode Roaster")
+st.write("Get Ready to be Roasted")
 
-# Input for Leetcode username
 username = st.text_input("Enter Leetcode username")
 
 API = "https://leetcode-stats-api.herokuapp.com/"
 
-if username is not None:
-    url = f"{API}{username}"  # Construct API URL
 
-    try:
-        # Make the GET request
-        response = requests.get(url)
-        data = response.json()  # Parse the JSON response
+url = f"{API}{username}"
+try:
 
-        # Check if the response is successful
-        if data["status"] == "success":
-            # Remove "SubmissionCalendar" key if it exists
-            if "submissionCalendar" in data:
-                del data["submissionCalendar"]
-            
-            # Create the roasting prompt
-            template = '''
-            Give me a most brutal and most dark humor, bad language roast on this Leetcode stats:
-            {response}
-            '''
-            prompt = PromptTemplate(template=template, input_variables=["response"])
-            formatted_prompt = prompt.format(response=data)  # Use parsed data
+    response = requests.get(url)
+    data = response.json() 
 
-            # Generate roast using the LLM
-            display = None
-            roast = llm.invoke(formatted_prompt)
-            
-            print(roast.content)
-            display = roast.content
-            # display = roast["content"]
-            st.write(display)
-        else:
-            st.error("Wrong Username! Please check again.")
-    except requests.exceptions.RequestException as e:
-        st.error(f"An error occurred: {e}")
+    if data["status"] == "success":
+
+        if "submissionCalendar" in data:
+            del data["submissionCalendar"]
+        
+
+        template = '''
+        Give me a most brutal and most dark humor, bad language roast on this Leetcode stats:
+        {response}
+        '''
+        prompt = PromptTemplate(template=template, input_variables=["response"])
+        formatted_prompt = prompt.format(response=data)  
+
+        st.write("-Generating may take some time-")
+        roast = llm.invoke(formatted_prompt)
+        
+        # print(roast.content)
+        display = roast.content
+        # display = roast["content"]
+        st.write(display)
+    else:
+        st.error("Wrong Username! Please check again.")
+except requests.exceptions.RequestException as e:
+    st.error(f"An error occurred: {e}")
